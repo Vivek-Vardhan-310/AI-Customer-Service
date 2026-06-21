38b2ffa18353d16961b19144c926a44803cd7498
Vivek-Vardhan-310 <vivekvardhan310@gmail.com>
2026-06-21 18:32:04 +0530

    trying to make voice chat work for the last time

--- frentend/vite.config.js
+++ frentend/vite.config.js
@@ -2,25 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/onnxruntime-web/dist/*.wasm',
-          dest: 'assets'
-        },
-        {
-          src: 'node_modules/onnxruntime-web/dist/*.mjs',
-          dest: 'assets'
-        },
-        {
-          src: 'node_modules/onnxruntime-web/dist/*.onnx',
-          dest: 'assets'
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['onnxruntime-web']
   }
